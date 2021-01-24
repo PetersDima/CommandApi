090df3d77888a2efb09461a5917b912f7a631a16
Dima Peters <petersdimawork@gmail.com>
2021-01-24 23:09:21 +0200

    Initial Commit

---
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CommandAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CommandsController : ControllerBase
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return new JsonResult(new {Name = "Dima", Message = "Hello world!"});
+        }
+        
+        [HttpGet("{name}")]
+        public ActionResult GreetingsByName(string name)
+        {
+            return new JsonResult(new {Name = name, Message = $"Hello {name}!"});
+        }
+    }
+}
