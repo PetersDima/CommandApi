94d8cdfdeaa007fe346cd2a7b1d6636b0c182929
Dima Peters <petersdimawork@gmail.com>
2021-02-10 03:19:05 +0200

    partially realized tests

---
+++ test/CommandAPI.Tests/CommandTests.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using CommandAPI.Models;
+using Xunit;
+
+namespace CommandAPI.Tests
+{
+    public class CommandTests : IDisposable
+    {
+        private Command _testCommand;
+        
+        public CommandTests()
+        {
+            //Arrange
+            _testCommand = new Command
+            {
+                HowTo = "Do some awesome",
+                Platform = "xUnit",
+                CommandLine = "dotnet test"
+            };
+        }
+        
+        [Fact]
+        public void CanChangeHowTo()
+        {
+            //Act
+            _testCommand.HowTo = "Execute Unit Tests";
+
+            //Assert
+            Assert.Equal("Execute Unit Tests", _testCommand.HowTo);
+        }
+        
+        [Fact]
+        public void CanChangePlatform()
+        {
+            //Act
+            _testCommand.Platform = ".Net Core";
+
+            //Assert
+            Assert.Equal(".Net Core", _testCommand.Platform);
+        }
+        
+        [Fact]
+        public void CanChangeCommandLine()
+        {
+            //Act
+            _testCommand.CommandLine = "dotnet run";
+
+            //Assert
+            Assert.Equal("dotnet run", _testCommand.CommandLine);
+        }
+
+        public void Dispose()
+        {
+            _testCommand = null;
+        }
+    }
+}
